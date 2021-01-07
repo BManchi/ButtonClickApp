92c3d7be29f81f0ce5bcdee651683ba47832193a
Bruno <b.manchinelli@gmail.com>
2021-01-07 11:00:13 -0300

    Progress

--- app/src/main/java/com/bmanchi/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/bmanchi/buttonclickapp/MainActivity.kt
@@ -2,10 +2,30 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+
+    private var textView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        button.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                textView?.append(userInput.text.toString() + "\n")
+            }
+        })
     }
 }
